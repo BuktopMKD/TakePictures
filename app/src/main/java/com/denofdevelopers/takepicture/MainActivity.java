03a328cc1eaea408947c42170d7e285dff16890c
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-08-01 15:15:07 +0200

    Implementation - Camera Intent and Permissions

--- app/src/main/java/com/denofdevelopers/takepicture/MainActivity.java
+++ app/src/main/java/com/denofdevelopers/takepicture/MainActivity.java
@@ -1,13 +1,79 @@
 package com.denofdevelopers.takepicture;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.image)
+    ImageView image;
+    @BindView(R.id.overlayText)
+    TextView overlayTest;
+
+    private static final int TAKE_PICTURE = 1111;
+    private static final int CAMERA_REQUEST_PERMISSION_CODE = 1112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onGoToCameraClick() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            requestCameraPermission();
+        } else {
+            startCameraIntent();
+        }
+    }
+
+    private void startCameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK) {
+            Bitmap thumbnail = (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
+            image.setImageBitmap(thumbnail);
+            overlayTest.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestCameraPermission() {
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_REQUEST_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCameraIntent();
+            }
+        }
     }
 }
